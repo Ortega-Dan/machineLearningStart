cc9bec6d84cd378916db6d6d8e792b0f5e824e6d
Dan Ortega <ortega.dan2010@gmail.com>
2022-09-22 22:30:33 -0500

    first reading and plotting

---
+++ airQualityAnalysis.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('dark_background')
+
+air_quality = pd.read_csv("air_quality_no2.csv", index_col=0, parse_dates=True)
+
+print(air_quality.head())
+
+air_quality["station_paris"].plot()
+plt.show()
