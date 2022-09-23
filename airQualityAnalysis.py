f924b7ef33be38116a591f19040f510f7bf3e227
Dan Ortega <ortega.dan2010@gmail.com>
2022-09-22 23:09:09 -0500

    working with default and dark styles exporting jupyter to html and printing pdf from html

--- airQualityAnalysis.py
+++ airQualityAnalysis.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use('dark_background')
+# plt.style.use('default')
 
 air_quality = pd.read_csv("air_quality_no2.csv", index_col=0, parse_dates=True)
 
 print(air_quality.head())
 
-air_quality["station_paris"].plot()
+# air_quality["station_paris"].plot()
+# plt.show()
+
+
+air_quality.plot.area(subplots=True)
 plt.show()
